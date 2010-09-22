24a1342e7a8ac9d1d018228162fee9f47cc477b7
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-09-22 17:28:36 -0400

    removing unnessesary steps

--- features/step_definitions/benchmark_steps.rb
+++
@@ -1,9 +0,0 @@
-Then /^the ingest time is output$/ do
-  start_event = Event.first(:name => "ingest started", :order => [ :id.desc ])
-  finish_event = Event.first(:name => "ingest finished", :order => [ :id.desc ])
-
-  start_time = Time.parse start_event.timestamp.to_s
-  finish_time = Time.parse finish_event.timestamp.to_s
-
-  puts "*** Ingest time: " + (finish_time - start_time).to_s
-end
